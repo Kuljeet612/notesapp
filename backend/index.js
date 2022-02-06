e77b6b9ea0f6becabe264038fbd87dc740ae3006
Kuljeet Kaur <kuljeet.kaur@oracle.com>
2022-02-06 20:18:00 +0530

    try catch block

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000  //changing to 5000 as 3000 will be used by the React app
 
 app.use(express.json())  //this is a middleware reqd so that we can access req.body in our models. this allows to send rwquests in JSON format
 //Available Routes
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,26 +2,40 @@ const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const { response } = require('express');
 
 //Create a user using POST "/api/auth/createuser". No login reqd
 router.post('/createuser', [
     body('name').isLength({min : 3}),
     body('email', "Enter a valid email").isEmail(),
     body('password', "Password must have at least 5 characters.").isLength({min : 5}),
-], (req, res) => {  
+], async(req, res) => {  
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
+    try {
+    //Check if email already exists
+    let user = await User.findOne({email: req.body.email});  //this will return a user having the email id passed
+
+    if(user) {
+        return res.status(400).json({error: "Sorry, a user with this email already exists"})
+    }   
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err)
-      res.json({error : "Please enter a unique value for email.", message: err.message})}) //to print on client
-        
+      })
+    //   .then(user => res.json(user))  //commenting as now we are using async await
+    //   .catch(err => {console.log(err)
+    //   res.json({error : "Please enter a unique value for email.", message: err.message})}) //to print on client
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 
